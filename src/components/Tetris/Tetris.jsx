604b2d04a0521262fb99ab9601470db888d2318f
AlbertoLanchas <albertolsn7@gmail.com>
2022-09-04 11:38:29 +0200

    Tetris new project
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

---
+++ src/components/Tetris/Tetris.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Tetris.css";
+
+import Board from "../Board/Board";
+import GameStats from "../GameStats/GameStats";
+
+import { useBoard } from "../../hooks/useBoard";
+import { useGameStats } from "../../hooks/useGameStats";
+
+const Tetris = ({ rows, columns, setGameOver }) => {
+  const [gameStats, addLinesCleared] = useGameStats();
+  const [board, setBoard] = useBoard({ rows, columns });
+  return (
+    <div className="Tetris">
+      <Board board={board} />
+      <GameStats gameStats={gameStats} />
+    </div>
+  );
+};
+
+export default Tetris;
