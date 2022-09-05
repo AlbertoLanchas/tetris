753d77f5a1c26c084313dfb9b892f7c8b2534745
AlbertoLanchas <albertolsn7@gmail.com>
2022-09-05 11:03:50 +0200

    interval time create and movement of tetrominoes
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

--- src/components/GameController/GameController.jsx
+++ src/components/GameController/GameController.jsx
@@ -3,6 +3,8 @@ import "./GameController.css";
 
 import { Action, actionForKey, actionIsDrop } from "../../business/Input";
 import { playerController } from "../../business/PlayerController";
+import { useInterval } from "../../hooks/useInterval";
+import { useDropTime } from "../../hooks/useDropTime";
 
 const GameController = ({
   board,
@@ -11,32 +13,37 @@ const GameController = ({
   setGameOver,
   setPlayer,
 }) => {
+  const [dropTime, pauseDropTime, resumeDropTime] = useDropTime({
+    gameStats,
+  });
+
+  useInterval(() => {
+    handleInput({ action: Action.SlowDrop });
+  }, dropTime);
+
   const onKeyUp = ({ code }) => {
     const action = actionForKey(code);
-    // if (actionIsDrop(action)) resumeDropTime();
-    if (action === Action.Quit) {
-      setGameOver(true);
-    }
+    if (actionIsDrop(action)) resumeDropTime();
   };
 
   const onKeyDown = ({ code }) => {
     const action = actionForKey(code);
-    handleInput({ action });
 
-    // if (action === Action.Pause) {
-    //   if (dropTime) {
-    //     pauseDropTime();
-    //   } else {
-    //     resumeDropTime();
-    //   }
-    // } else if (action === Action.Quit) {
-    //   setGameOver(true);
-    // } else {
-    //   if (actionIsDrop(action)) pauseDropTime();
-    //   if (!dropTime) {
-    //     return;
-    //   }
-    // }
+    if (action === Action.Pause) {
+      if (dropTime) {
+        pauseDropTime();
+      } else {
+        resumeDropTime();
+      }
+    } else if (action === Action.Quit) {
+      setGameOver(true);
+    } else {
+      if (actionIsDrop(action)) pauseDropTime();
+      if (!dropTime) {
+        return;
+      }
+      handleInput({ action });
+    }
   };
 
   const handleInput = ({ action }) => {
