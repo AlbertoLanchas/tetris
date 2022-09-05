8c3e7ad3ac105eb1bfe8a5846cf85cdbeb06bf25
AlbertoLanchas <albertolsn7@gmail.com>
2022-09-05 13:58:14 +0200

    btn play & tetris logo
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./Menu.css";
+import logo from "../../assets/logo.png";
 
-const Menu = ({ onClick }) => {
+const Menu = ({ onClick, img }) => {
   return (
     <div className="Menu">
+      <img className="Image" src={logo} alt="tetris logo" />
       <button className="Button" onClick={onClick}>
-        Play Tetris!
+        <a href="#" className="btn-neon">
+          <span id="span1"></span>
+          <span id="span2"></span>
+          <span id="span3"></span>
+          <span id="span4"></span>
+          PLAY
+        </a>
       </button>
     </div>
   );
